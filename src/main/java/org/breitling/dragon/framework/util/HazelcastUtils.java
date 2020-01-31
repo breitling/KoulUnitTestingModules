209a266340e5ad6a43ac259b3b76d2da13fb738a
Bob Breitling <bobb@sg360.com>
2020-01-31 12:52:16 -0600

    Added support for Hazelcast instances

---
+++ src/main/java/org/breitling/dragon/framework/util/HazelcastUtils.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.breitling.dragon.framework.util;
+
+import com.hazelcast.config.JoinConfig;
+import com.hazelcast.config.NetworkConfig;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+
+/**
+ *
+ * @author Bob Breitling
+ */
+public class HazelcastUtils
+{
+    private static String groupName;
+    private static String groupPassword;
+    
+    private HazelcastUtils(){
+    }
+    
+    public static HazelcastInstance createTestInstance()
+    {
+        groupName = "TestDataGrid";
+        groupPassword = "T3st4A11";
+        
+        return Hazelcast.newHazelcastInstance(hazelCastConfigBean());
+    }
+    
+    public static HazelcastInstance createTestInstance(String name, String password)
+    {
+        groupName = name;
+        groupPassword = password;
+        
+        return Hazelcast.newHazelcastInstance(hazelCastConfigBean());
+    }
+
+//  PRIVATE METHODS
+    
+    @SuppressWarnings("deprecation")
+    private static com.hazelcast.config.Config hazelCastConfigBean()
+    {
+        com.hazelcast.config.Config config = new com.hazelcast.config.Config();
+        
+        config.setInstanceName("TestDataGrid");
+        config.getGroupConfig().setName(groupName);
+        config.getGroupConfig().setPassword(groupPassword);
+        
+        NetworkConfig network = config.getNetworkConfig();
+        network.setPort(5701).setPortCount(10);
+        network.setPortAutoIncrement(true);
+        
+        JoinConfig join = network.getJoin();
+        join.getMulticastConfig().setEnabled(true);
+        join.getAwsConfig().setEnabled(false);;
+        
+        return config; 
+    }
+}
